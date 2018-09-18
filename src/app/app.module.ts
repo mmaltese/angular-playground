0db586c5d949620927c08be8bf4d63f29004ebc1
Mare <marimare@gmail.com>
2018-09-18 12:45:48 +0200

    Updating AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http'; 
 
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
-import 'hammerjs';
 
 @NgModule({
   declarations: [
